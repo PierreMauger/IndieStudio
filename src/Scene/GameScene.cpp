49f194672a8658577f1453f09d417ed836a82d94
NairodGH <dorian.ayoul@epitech.eu>
2022-06-07 14:41:48 +0200

    [ADD] local changes to save before master checkout

--- src/Scene/GameScene.cpp
+++ src/Scene/GameScene.cpp
@@ -11,29 +11,7 @@ using namespace neo;
 
 GameScene::GameScene(std::shared_ptr<MessageBus> messageBus)
 {
-    const std::vector<std::string> tmpMap = {
-                {"###########"},
-                {"#1 WWWWW  #"},
-                {"#  #W#W#  #"},
-                {"#WWWWWWWWW#"},
-                {"##W#W#W#W##"},
-                {"#WWWWWWWWW#"},
-                {"##W#####W##"},
-                {"#WWWWWWWWW#"},
-                {"#  #W#W#  #"},
-                {"#  WWWWW 2#"},
-                {"###########"}
-    };
-
     this->_messageBus = messageBus;
-    this->_players.insert(std::make_pair(0, std::make_unique<Player>("RoboCat", glm::vec3(0.f), glm::vec3(0.2f))));
-    this->_players.insert(std::make_pair(1, std::make_unique<Player>("RoboCat", glm::vec3(0.f), glm::vec3(0.2f))));
-    for (int i = 0, mapId = 0; i < tmpMap.size(); i++) {
-        for (int j = 0; j < tmpMap[i].size(); j++) {
-            if (tmpMap[i][j] == '#')
-                _map.insert(std::make_pair(mapId++, std::make_unique<Wall>("Cube", glm::vec3(i * 0.2f, j * 0.2f, 0), false, glm::vec3(0.2f))));
-        }
-    }
 }
 
 GameScene::~GameScene()
@@ -59,9 +37,40 @@ void GameScene::update()
 
 void GameScene::loadScene()
 {
+    const std::vector<std::string> tmpMap = {
+                {"###########"},
+                {"#0 WWWWW  #"},
+                {"#  #W#W#  #"},
+                {"#WWWWWWWWW#"},
+                {"##W#W#W#W##"},
+                {"#WWWWWWWWW#"},
+                {"##W#W#W#W##"},
+                {"#WWWWWWWWW#"},
+                {"#  #W#W#  #"},
+                {"#  WWWWW 1#"},
+                {"###########"}
+    };
+    glm::vec3 pos(0.f);
     Packet packet;
     int i = 0;
 
+    for (int i = 0, mapId = 0, screenWidth = GetScreenWidth(), screenHeight = GetScreenHeight(); i < tmpMap.size(); i++) {
+        for (int j = 0; j < tmpMap[i].size(); j++) {
+            printf("%f %f %d %d %f %f\n", pos.x, pos.y, screenWidth, screenHeight, (float)screenWidth / tmpMap[0].size(), (float)screenHeight / tmpMap.size());
+            if (tmpMap[i][j] == '#')
+                _map.insert(std::make_pair(mapId++, std::make_unique<Wall>("Block", pos, false,
+                    glm::vec3(screenWidth / tmpMap[0].size(), screenHeight / tmpMap.size(), 1.f))));
+            if (tmpMap[i][j] == 'W')
+                _map.insert(std::make_pair(mapId++, std::make_unique<Wall>("Wall", pos, true,
+                    glm::vec3(screenWidth / tmpMap[0].size(), screenHeight / tmpMap.size(), 1.f))));
+            if (tmpMap[i][j] >= '0' && tmpMap[i][j] <= '9')
+                this->_players.insert(std::make_pair(tmpMap[i][j] - '0', std::make_unique<Player>("RoboCat", pos,
+                    glm::vec3(screenWidth / tmpMap[0].size(), screenHeight / tmpMap.size(), 1.f))));
+            pos.x += screenWidth / tmpMap[0].size();
+        }
+        pos.x = 0.f;
+        pos.y += screenHeight / tmpMap.size();
+    }
     for (auto& player : this->_players)
         packet << player.second->getType() << i++ << *player.second;
     for (auto& wall : this->_map)
@@ -74,16 +83,16 @@ void GameScene::handleKeyPressed(int playerNb, std::string action)
     if (this->_players.find(playerNb) == this->_players.end())
         return;
     if (action == "MoveRight") {
-        this->_players[playerNb]->addX(0.1f);
+        this->_players[playerNb]->addX(10.f);
         this->_players[playerNb]->setRotation(90.f);
     } else if (action == "MoveLeft") {
-        this->_players[playerNb]->addX(-0.1f);
+        this->_players[playerNb]->addX(-10.f);
         this->_players[playerNb]->setRotation(270.f);
     } else if (action == "MoveUp") {
-        this->_players[playerNb]->addY(0.1f);
+        this->_players[playerNb]->addY(10.f);
         this->_players[playerNb]->setRotation(180.f);
     } else if (action == "MoveDown") {
-        this->_players[playerNb]->addY(-0.1f);
+        this->_players[playerNb]->addY(-10.f);
         this->_players[playerNb]->setRotation(0.f);
     }
 }
@@ -91,11 +100,11 @@ void GameScene::handleKeyPressed(int playerNb, std::string action)
 void GameScene::handleKeyReleased(int playerNb, std::string action)
 {
     if (action == "MoveRight")
-        this->_players[playerNb]->addX(-0.1f);
+        this->_players[playerNb]->addX(-10.f);
     else if (action == "MoveLeft")
-        this->_players[playerNb]->addX(0.1f);
+        this->_players[playerNb]->addX(10.f);
     else if (action == "MoveUp")
-        this->_players[playerNb]->addY(-0.1f);
+        this->_players[playerNb]->addY(-10.f);
     else if (action == "MoveDown")
-        this->_players[playerNb]->addY(0.1f);
+        this->_players[playerNb]->addY(10.f);
 }
